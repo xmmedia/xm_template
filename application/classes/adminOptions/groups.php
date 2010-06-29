87673ceec77386ee12df2caa762a5971bc1b0658
Dan Hulton <dan@danhulton.com>
2010-06-29 01:27:35 +0000

    AdminOptions now inherits from DefaultOverridable, to allow it to have $view_folder and $url_base properties.

--- application/classes/adminOptions/groups.php
+++ application/classes/adminOptions/groups.php
@@ -5,21 +5,31 @@
  */
 class AdminOptions_Groups extends AdminOptions_Default {
     /**
-     * Constructor.
+     * Constructor, sets defaults.
+     *
+     * @param array $options The options to set.
      */
-    public function __construct() {
+    public function __construct($options = array()) {
+        parent::__construct($options);
+        
+        // The following are not options to be overridden, but intrinsic to the workings of the class.
+        
         $this->jelly_model      = 'group';
         $this->url_model_name   = 'groups';
         $this->display_name     = 'Groups';
         
-        $url_prefix = "/" . Kohana::config('claero-admin.url-prefix') . "/" . $this->url_model_name . "/";
+        $url_prefix = $this->url_base . $this->url_model_name . "/";
         
-        $this->list_columns = array(
-            'name'          => new Column_List_Display('name'),
-            'description'   => new Column_List_Display('description'),
-            'roles'         => new Column_List_Display_Collection('roles'),
-            'edit'          => new Column_List_Action_Image('edit', $url_prefix, array('source' => '/images/icons/page_white_edit.png')),
-            'delete'        => new Column_List_Action_Image('delete', $url_prefix),
+        $this->list_fields = array(
+            'name'          => new Field_List_Display('name'),
+            'description'   => new Field_List_Display('description'),
+            'roles'         => new Field_List_Display_Collection('roles'),
+            'edit'          => new Field_List_Action_Image('edit', $url_prefix, array('source' => '/images/icons/page_white_edit.png')),
+            'delete'        => new Field_List_Action_Image('delete', $url_prefix),
+        );
+        
+        $this->edit_fields = array(
+            'name'          => new Field_Edit_Auto(),
         );
     }
 }
--- application/classes/adminOptions/users.php
+++ application/classes/adminOptions/users.php
@@ -5,14 +5,20 @@
  */
 class AdminOptions_Users extends AdminOptions_Default {
     /**
-     * Constructor.
+     * Constructor, sets defaults.
+     *
+     * @param array $options The options to set.
      */
-    public function __construct() {
+    public function __construct($options = array()) {
+        parent::__construct($options);
+        
+        // The following are not options to be overridden, but intrinsic to the workings of the class.
+        
         $this->jelly_model      = 'user';
         $this->url_model_name   = 'users';
         $this->display_name     = 'Users';
         
-        $url_prefix = "/" . Kohana::config('claero-admin.url-prefix') . "/" . $this->url_model_name . "/";
+        $url_prefix = $this->url_base . $this->url_model_name . "/";
         
         $this->list_columns = array(
             'email'     => new Column_List_Display('email'),
