a137e94d7cb06f016ee17a3c3e75678dc61bc80e
Dan Hulton <dan@danhulton.com>
2010-07-03 21:40:23 +0000

    Rolled back to old "users" file, since it's helpful to have one of the "old style" still working.  Groups public controller can now work with the rest of the template!  Requires auth, sets section, all those neat things!

--- application/classes/controller/admin/groups.php
+++ application/classes/controller/admin/groups.php
@@ -2,6 +2,17 @@
 
 class Controller_Admin_Groups extends Controller_Template {
     /**
+     * Restrict access and define section.
+     */
+    public function before() {
+        $this->roles_required = array('Edit Groups');
+        
+        $this->section = 'groups';
+        
+        parent::before();
+    }
+    
+    /**
      * Display the main page of the administration area
      */
 	public function action_index() {
