54ced98dde6b42dd3d2c226b86046a3f7e011d3b
Dan Hulton <dan@danhulton.com>
2010-07-04 21:52:29 +0000

    Changed "View" link for Groups to an Action_Image.  Implemented "users" as a ClaeroAdmin-controlled admin section.

--- application/classes/controller/admin/users.php
+++ application/classes/controller/admin/users.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Admin_Users extends Controller_Template {
+class Controller_Admin_users extends Controller_Template {
     /**
      * Restrict access and define section.
      */
@@ -13,168 +13,43 @@ class Controller_Admin_Users extends Controller_Template {
     }
     
     /**
-     * Display the list of users.
+     * Display a list of all users.
      */
 	public function action_index() {
-        $users = Jelly::select('user')->execute();
-        
-        $this->template->content = View::factory(
-            'admin/users/index',
-            array(
-                'users' => $users
-            )
-        );
+		$this->template->content = Request::factory('claero-admin/users')->execute()->response;
 	}
     
-    /**
-     * Edit a user.
-     */
-    public function action_edit($id) {
-        try {
-            $groups = Jelly::select('group')->execute();
-            
-            // If form was submitted
-            if ('POST' == Request::$method) {
-                // If no checkboxes selected, field doesn't even show up in post, must be set
-                $_POST['groups'] = isset($_POST['groups']) ? $_POST['groups'] : array();
-                
-                // Build user from form
-                $user = Jelly::factory(
-                    'user',
-                    Arr::extract(
-                        $_POST,
-                        array(
-                            'id',
-                            'email',
-                            'name',
-                            'password',
-                            'password_confirm',
-                            'groups'
-                        )
-                    )
-                );
-                
-                // Save (involves multiple tables, wrap in transaction)
-                DB::query(null, 'START TRANSACTION')->execute();
-                $user->save($_POST['id']);
-                DB::query(null, 'COMMIT')->execute();
-                
-                Flash::set('message', __("User ':user' saved.", array(':user' => $user->name)));
-                $this->request->redirect('/admin/users');
-            }
-            // If form has not been submitted
-            else {
-                // Build user from ID
-                $user = Jelly::select('user', $id);
-            }
-        }
-        // Error while validating form
-        catch (Validate_Exception $e) {
-            $this->template->message = $this->get_error_messages($e, 'users');
-        }
-        // Other error
-        catch (Exception $e) {
-            $this->template->message = __("Could not edit user.");
-            Fire::error($e);
-        }
-        
-        $this->template->content = View::factory(
-            'admin/users/details',
-            array(
-                'title'         => __("Edit User"),
-                'action'        => "/admin/users/edit/$id",
-                'id'            => $id,
-                'user'          => $user,
-                'groups'        => $groups,
-                'user_groups'   => arr::pluck('id', $user->groups->as_array())
-            )
-        );
-    }
-    
-    /**
-     * Delete a user.
-     */
-    public function action_delete($id) {
-        try {
-            // Get name
-            $user = Jelly::select('user')->load($id);
-            $name  = $user->name;
-            
-            // Delete user
-            $user->delete();
-            
-            Flash::set('message', __("Group ':user' has been deleted.", array(':user' => $name)));
-        }
-        // Couldn't delete user.
-        catch (Exception $e) {
-            Flash::set('message', __("Could not delete user."));
-        }
-        
-        $this->request->redirect('/admin/users');
-    }
-    
-    /**
-     * Create a new user.
-     */
-    public function action_new() {
-        try {
-            $groups = Jelly::select('group')->execute();
-            
-            // If form was submitted
-            if ('POST' == Request::$method) {
-                // If no checkboxes selected, field doesn't even show up in post, must be set
-                $_POST['groups'] = isset($_POST['groups']) ? $_POST['groups'] : array();
-                
-                // Build user from form
-                $user = Jelly::factory(
-                    'user',
-                    Arr::extract(
-                        $_POST,
-                        array(
-                            'id',
-                            'email',
-                            'name',
-                            'password',
-                            'password_confirm',
-                            'groups'
-                        )
-                    )
-                );
-                
-                // Save (involves multiple tables, wrap in transaction)
-                DB::query(null, 'START TRANSACTION')->execute();
-                $user->save();
-                DB::query(null, 'COMMIT')->execute();
-                
-                Flash::set('message', __("User ':user' created.", array(':user' => $user->name)));
-                $this->request->redirect('/admin/users');
-            }
-            // If form has not been submitted
-            else {
-                // Build blank
-                $user = Jelly::factory('user');
-            }
-        }
-        // Error while validating form
-        catch (Validate_Exception $e) {
-            $this->template->message = $this->get_error_messages($e, 'users');
-        }
-        // Other error
-        catch (Exception $e) {
-            $this->template->message = __("Could not create new user.");
-            Fire::error($e);
-        }
-        
-        $this->template->content = View::factory(
-            'admin/users/details',
-            array(
-                'title'         => __("New User"),
-                'action'        => '/admin/users/new',
-                'id'            => '',
-                'user'          => $user,
-                'groups'        => $groups,
-                'user_groups'   => array()
-            )
-        );
-    }
+	/**
+	 * Edits a user.
+	 *
+	 * @param integer $id The ID of the user to edit.
+	 */
+	public function action_edit($id) {
+		$this->template->content = Request::factory("claero-admin/users/edit/$id")->execute()->response;
+	}
+	
+	/**
+	 * Creates a new user.
+	 */
+	public function action_new() {
+		$this->template->content = Request::factory("claero-admin/users/new")->execute()->response;
+	}
+	
+	/**
+	 * Views a user.
+	 *
+	 * @param integer $id The ID of the user to view.
+	 */
+	public function action_view($id) {
+		$this->template->content = Request::factory("claero-admin/users/view/$id")->execute()->response;
+	}
+	
+	/**
+	 * Deletes a user.
+	 *
+	 * @param integer $id The ID of the user to delete.
+	 */
+	public function action_delete($id) {
+		Request::factory("claero-admin/users/delete/$id")->execute();
+	}
 }
--- application/classes/controller/claeroAdmin/users.php
+++ application/classes/controller/claeroAdmin/users.php
@@ -10,10 +10,20 @@ class Controller_ClaeroAdmin_Users extends Controller_ClaeroAdmin_Claero_Default
         parent::__construct($request);
         
         $this->model('user')
+			 ->identifier_label_field('name')
 			 ->list_fields(array(
                 'name'          => new Field_List_Display,
 				'email'			=> new Field_List_Display,
-                'delete'        => new Field_List_Action,
+                'groups'        => new Field_List_Display_Collection,
+                'view'          => new Field_List_Action_Image(array('source' => '/images/icons/magnifier.png')),
+                'edit'          => new Field_List_Action_Image(array('source' => '/images/icons/page_white_edit.png')),
+                'delete'        => new Field_List_Action_Image,
+             ))
+             ->edit_fields(array(
+                'id'            => new Field_Edit_Hidden,
+                'name'          => new Field_Edit_Text,
+                'email'   		=> new Field_Edit_Text,
+                'groups'        => new Field_Edit_Checkboxes,
              ));
     }
 }
