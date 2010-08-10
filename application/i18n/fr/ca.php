2f379082e3f103ab506d8a32b9d89123a8812a5a
Craig Nakamoto <craig.nakamoto@claero.com>
2010-08-10 23:15:39 +0000

    getting there, adding some sample pages, simplifying page code, etc.

---
+++ application/i18n/fr/ca.php
@@ -0,0 +1,93 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+ 
+return array
+(
+    
+    // sample menu items
+    
+    'About' => 'À propos de Nous',
+    'News' => 'Nouvelles',
+    'Contact' => 'Contactez-Nous',
+    'Login' => 'Login',
+    'Overview' => "Vue d'ensemble",
+    'Our values' => 'Nos valeurs',
+    'Our guiding principles' => 'Principes directeurs',
+    'Testimonials' => 'Témoignages',
+
+    // misc
+    'Welcome' => 'Bienvenue',
+    'No translation was available, we apologize for any inconvenience. Here is the Canadian English version:' => 
+        'Aucune traduction était disponible, nous nous excusons pour tout inconvénient. Voici la version canadienne-anglaise:',
+    'Unfortunately this page is unavailable. We apologize for any inconvenience.' =>
+        "Malheureusement, cette page n'est pas disponible. Nous nous excusons pour tout inconvénient.",
+    'Wines of people, place and time' => 'Vins de terroir, d’époque et d’humanité',
+    'Unfortunately this page has no public content yet. We apologize for any inconvenience.' => 
+        "Malheureusement, cette page n'a pas de contenu pour le moment. Nous nous excusons pour tout inconvénient.",
+    'This page has no content.  Since you are logged in and have permissions, you can create it now:' => 
+        "Cette page n'a pas de contenu. Puisque vous êtes connecté et avoir les autorisations, vous pouvez le créer maintenant:",
+            
+    // footer
+    'About this site' => 'À propos de ce site',
+    'Site Map' => 'Plan du Site',
+    'Privacy Policy' => 'Confidentialité',
+    'All Rights Reserved' => 'Tous Droits Réservés',
+    
+    // edit page form
+    'Edit Page' => 'Éditer',
+    'Generic Page Properties' => 'Propriétés Page Web Génériques',
+    'Section' => 'Section',
+    'Short Name' => 'Nom Court',
+    'Name' => 'Nom',
+    'Start Publishing Date' => 'Début de publication',
+    'End Publishing Date' => 'Fin de publication',
+    'Canadian English (en-ca)' => 'Canadien-anglais (fr-ca)',
+    'Canadian French (fr-ca)' => 'Canadien-français (fr-ca)',
+    'Title' => 'Titre',
+    'HTML Content' => 'Contenu HTML',
+    'META Description' => 'Description META',
+    'META Keywords' => 'Mots-clés META',
+    'Submit' => 'Soumettre',
+    'Save' => 'Sauver',
+    'Publish' => 'Publier',
+    'Edit' => 'Éditer',
+    
+
+    // other misc
+    //'more' => '',
+    //'read more' => '',
+    'First' => 'Premier',
+    'Next' => 'Prochain',
+    'Previous' => 'Précédent',
+    'Last' => 'Bout',
+    'HOME' => "ACCUEIL",
+    'Home' => "Accueil",
+    'DIRECTORY' => 'ANNUAIRE',
+    'Directory' => 'Annuaire',
+    'NEWS' => 'NOUVELLES',
+    'News' => 'Nouvelles',
+    'PHOTOS' => 'PHOTOS',
+    'Photos' => 'Photos',
+    'LINKS' => 'LIENS',
+    'Links' => 'Liens',
+    'PROJECTS' => 'PROJETS',
+    'Projects' => 'Projets',
+    'ALL RIGHTS RESERVED' => 'TOUS DROITS RÉSERVÉS',
+	'Name' => 'Nom',
+    'Phone' => 'Téléphone',
+    'Address' => 'Addresse',
+    'Internet' => 'Internet',
+    'All' => 'Tout',
+    'First Name' => 'Prénom',
+    'Last Name' => 'Nom de Famille',
+    'Search' => 'Recherche',
+    'Clear search' => 'Dégagez',
+    'Print version' => "Version d'impression",
+    'Refresh data' => 'Régénérez',
+    'Add new record' => 'Ajouter un nouvel enregistrement',
+    'ADD' => 'AJOUTER',
+    'ADD NEW' => 'AJOUTER NOUVEAU',
+    'EDIT' =>'EDIT',
+    'DELETE' => 'EFFACER',
+    
+    
+); //End of application/i18n/fr/ca.php
