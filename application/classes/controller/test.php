52cf36b817740ead87fc25fb680221ad8cb36a88
Craig Nakamoto <craig.nakamoto@claero.com>
2010-08-24 21:24:49 +0000

    working on form generation

--- application/classes/controller/test.php
+++ application/classes/controller/test.php
@@ -7,8 +7,9 @@ class Controller_Test extends Controller_Base {
 
     public function action_index() {
     
-    
-        $this->template->bodyHtml .= ORM::factory('user',2)->get_html();
+    	
+    	 $this->template->bodyHtml .= '<style>.required {color:red;}</style>' . EOL;
+    	 $this->template->bodyHtml .= ORM::factory('user',2)->get_html();
         
     
         //$this->template->bodyHtml .= Kohana::debug(Kohana::config('production'));
