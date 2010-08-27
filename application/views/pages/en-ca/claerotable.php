00c1afec0531e4b23c07c8baa28e6f1686e1fa41
Darryl Hein <dhein@xmmedia.com>
2010-08-27 15:14:05 +0000

    updated html5 reset css code a few additions for testing claero table

--- application/views/pages/en-ca/claerotable.php
+++ application/views/pages/en-ca/claerotable.php
@@ -1,21 +1,31 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<style>
+.claero_table tr td { padding:3px; width:75px; }
+</style>
 <?php try {
 
 $test_data = array();
-$test_data[] = array('row1col1', 'row1col2', 'row1col3', 'row1col4', 'row1col5');
-$test_data[] = array('row2col1', 'row2col2', 'row2col3', 'row2col4', 'row2col5');
-$test_data[] = array('row3col1', 'row3col2', 'row3col3', 'row3col4', 'row3col5');
-$test_data[] = array('row4col1', 'row4col2', 'row4col3', 'row4col4', 'row4col5');
+$test_data[] = array('row1 col1', 'row1 col2', 'row1 col3', 'row1 col4', 'row1 col5');
+$test_data[] = array('row2 col1', 'row2 col2', 'row2 col3', 'row2 col4', 'row2 col5');
+$test_data[] = array('row3 col1', 'row3 col2', 'row3 col3', 'row3 col4', 'row3 col5');
+$test_data[] = array('row4 col1', 'row4 col2', 'row4 col3', 'row4 col4', 'row4 col5');
 
 $table = new Claero_Table(array(
-	'heading' => array('Col 1', 'Col 2', 'Col 3', 'Col 4', 'Col 5')
+	'table_attributes' => array(
+		'id' => 'blah',
+		'class' => 'claero_table',
+	),
+	'heading' => array('Col 1', 'Col 2', 'Col 3', 'Col 4', 'Col 5'),
+	'data' => $test_data,
 ));
 
 // add the data rows to the table
-foreach ($test_data as $data_row) {
+/*foreach ($test_data as $data_row) {
     $table->add_row($data_row);
-} // foreach
+} // foreach*/
 
-echo $table;
+echo $table->get_html();
 
 ?>
 
