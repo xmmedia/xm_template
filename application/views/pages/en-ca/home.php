4c3660bfd9e64b9fd455b64af720078ff77e6368
Darryl Hein <dhein@xmmedia.com>
2010-10-14 02:56:38 +0000

    first wave of changes to clean up the CSS and add some formatting

--- application/views/pages/en-ca/home.php
+++ application/views/pages/en-ca/home.php
@@ -7,3 +7,10 @@
 </ul>
 <p>If you are using Firefox, you need to have Firebug and FirePHP add-ons installed. Once this is done, you should be able to
 open firebug and see the debugging and benchmark data in the console. Sometimes you have to restart the browser to make this work.</p>
+
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
