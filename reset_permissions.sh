d83adcb334c8e88d924e9f23117ad2f272131c87
Darryl Hein <dhein@xmmedia.com>
2010-11-16 23:31:49 +0000

    removing sudo from reset_permissions.sh

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -1,15 +1,15 @@
-sudo chown templat4 -R *
-sudo chgrp templat4 -R *
-sudo chgrp apache -R uploads
-sudo chgrp apache -R html/uploads
-sudo chgrp apache -R sessions
-sudo chgrp apache -R logs
-sudo chgrp apache -R cache
+chown templat4 -R *
+chgrp templat4 -R *
+chgrp apache -R uploads
+chgrp apache -R html/uploads
+chgrp apache -R sessions
+chgrp apache -R logs
+chgrp apache -R cache
 chmod g+w -R uploads
 chmod g+w -R html/uploads
 chmod g+w -R sessions
 chmod g+w -R logs
 chmod g+w -R cache
 cd html
-sudo find . -type f -exec chmod 644 {} \;
-sudo find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
