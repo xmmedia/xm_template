eaf36f4b2857fbdd6e138315e0d059a0850bf534
Darryl Hein <dhein@xmmedia.com>
2010-11-16 08:22:55 +0000

    code for using old md5'd passwords

---
+++ application/config/auth.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+return array(
+	// uncomment this if you want the system to use md5 (vs default of sha1)
+	// also comment out the function in application/class/auth/orm.php to not use a salt
+	//'hash_method'    => 'md5',
+);
