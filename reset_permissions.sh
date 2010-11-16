3fca79bbf524dc77be57430756a8e41a0b50607a
Darryl Hein <dhein@xmmedia.com>
2010-11-16 23:36:37 +0000

    removing sessions folder

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -2,12 +2,10 @@ chown templat4 -R *
 chgrp templat4 -R *
 chgrp apache -R uploads
 chgrp apache -R html/uploads
-chgrp apache -R sessions
 chgrp apache -R logs
 chgrp apache -R cache
 chmod g+w -R uploads
 chmod g+w -R html/uploads
-chmod g+w -R sessions
 chmod g+w -R logs
 chmod g+w -R cache
 cd html
