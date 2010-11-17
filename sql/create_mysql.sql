1895463068219fc79bdf274c80b995d3438ade4e
Darryl Hein <dhein@xmmedia.com>
2010-11-17 00:31:03 +0000

    updating the create sql

--- sql/create_mysql.sql
+++ sql/create_mysql.sql
@@ -3,20 +3,14 @@
 -- http://www.phpmyadmin.net
 --
 -- Host:
--- Generation Time: Nov 09, 2010 at 11:42 PM
--- Server version: 5.1.50
+-- Generation Time: Nov 16, 2010 at 05:29 PM
+-- Server version: 5.0.77
 -- PHP Version: 5.2.14
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `templat4_core`
+-- Database: `bvccxmme_main`
 --
 
 -- --------------------------------------------------------
@@ -26,21 +20,22 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE `auth_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
-  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(100) collate utf8_unicode_ci NOT NULL,
   `access_time` datetime NOT NULL,
   `auth_type_id` int(11) NOT NULL,
-  `browser` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `ip_address` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
+  `browser` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `ip_address` varchar(15) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`),
   KEY `user_id` (`user_id`),
   KEY `access_type_id` (`auth_type_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `auth_log`
 --
+
 
 -- --------------------------------------------------------
 
@@ -49,10 +44,10 @@ CREATE TABLE `auth_log` (
 --
 
 CREATE TABLE `auth_type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(30) collate utf8_unicode_ci NOT NULL,
   `display_order` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `display_order` (`display_order`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -74,25 +69,26 @@ INSERT INTO `auth_type` VALUES(6, 'Too Many Attempts', 6);
 --
 
 CREATE TABLE `claero_change` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
-  `event_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `sql` varchar(4096) COLLATE utf8_unicode_ci NOT NULL,
-  `table_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `event_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `sql` varchar(4096) collate utf8_unicode_ci NOT NULL,
+  `table_name` varchar(64) collate utf8_unicode_ci NOT NULL,
   `record_id` int(11) NOT NULL,
-  `query_type` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `query_type` varchar(12) collate utf8_unicode_ci NOT NULL,
   `row_count` int(11) NOT NULL,
   `query_time` decimal(10,3) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `user_id` (`user_id`),
   KEY `table_name` (`table_name`),
   KEY `query_type` (`query_type`),
   KEY `record_id` (`record_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `claero_change`
 --
+
 
 -- --------------------------------------------------------
 
@@ -101,24 +97,24 @@ CREATE TABLE `claero_change` (
 --
 
 CREATE TABLE `demo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `text` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL auto_increment,
+  `text` varchar(50) collate utf8_unicode_ci NOT NULL,
   `checkbox` tinyint(1) NOT NULL,
   `date` date NOT NULL,
   `datetime` datetime NOT NULL,
   `gender` tinyint(1) NOT NULL,
-  `hidden` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `hidden` varchar(10) collate utf8_unicode_ci NOT NULL,
+  `password` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `phone` varchar(32) collate utf8_unicode_ci NOT NULL,
   `radio` tinyint(1) NOT NULL,
   `demo_sub_id` int(11) NOT NULL,
-  `textarea` text COLLATE utf8_unicode_ci NOT NULL,
+  `textarea` text collate utf8_unicode_ci NOT NULL,
   `yes_no` tinyint(1) NOT NULL,
-  `public_filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `public_original_filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `private_filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `private_original_filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `public_filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `public_original_filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `private_filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `private_original_filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -127,6 +123,7 @@ CREATE TABLE `demo` (
 
 INSERT INTO `demo` VALUES(2, 'Text', 0, '2010-10-14', '2010-10-19 08:58:45', 2, 'blah 1', '', '011-554-524-9876-123', 2, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu risus in eros aliquam ultricies nec sit amet purus. Nulla et nibh eros. Integer id lacus in risus auctor iaculis. Etiam rhoncus suscipit feugiat. Vivamus vitae pulvinar risus. Nulla porta turpis in purus consequat dictum. Sed et augue dolor, sed vulputate felis. Duis at nisi tortor, vitae venenatis arcu. Etiam vel cursus augue. Donec at mauris nunc. Morbi egestas dolor ac massa tincidunt sit amet malesuada elit porta. Nullam elit purus, tincidunt ac gravida sit amet, vestibulum et ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer risus lectus, malesuada vitae pharetra mollis, accumsan non tortor. Mauris at diam in elit rhoncus volutpat nec at neque. Aenean nec augue sit amet lacus luctus convallis sed et odio. Proin facilisis, lorem eu placerat vulputate, odio elit pulvinar urna, in tincidunt massa neque eget lorem.', 2, '1289109933_001.jpg', '001.jpg', '027.jpg', '027.jpg');
 INSERT INTO `demo` VALUES(3, 'Text 2', 1, '0000-00-00', '0000-00-00 00:00:00', 1, '', '', '----', 0, 1, 'a big bunch of text!', 0, '', '', '', '');
+INSERT INTO `demo` VALUES(7, 'Test', 1, '2010-11-10', '0000-00-00 00:00:00', 0, '', '', '1-613-744-7011-123', 2, 0, '', 0, '1289405446_dsc09926c-400.png', 'DSC09926c-400.png', '20101022-nakamoto-resume.pdf', '20101022-nakamoto-resume.pdf');
 
 -- --------------------------------------------------------
 
@@ -135,11 +132,11 @@ INSERT INTO `demo` VALUES(3, 'Text 2', 1, '0000-00-00', '0000-00-00 00:00:00', 1
 --
 
 CREATE TABLE `demo_sub` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `expiry_date` datetime NOT NULL,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) collate utf8_unicode_ci NOT NULL,
   `display_order` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   KEY `date_expired` (`id`,`expiry_date`,`display_order`,`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -150,7 +147,9 @@ CREATE TABLE `demo_sub` (
 INSERT INTO `demo_sub` VALUES(1, '0000-00-00 00:00:00', 'Option 1', 3);
 INSERT INTO `demo_sub` VALUES(2, '0000-00-00 00:00:00', 'Option 2', 2);
 INSERT INTO `demo_sub` VALUES(4, '2010-11-04 23:22:57', 'Option 3', 3);
-INSERT INTO `demo_sub` VALUES(5, '2010-11-04 23:23:21', 'Options 4', 3);
+INSERT INTO `demo_sub` VALUES(5, '2010-11-04 23:23:21', 'Options 3', 3);
+INSERT INTO `demo_sub` VALUES(6, '0000-00-00 00:00:00', 'Option 3', 3);
+INSERT INTO `demo_sub` VALUES(7, '0000-00-00 00:00:00', 'Option 4', 4);
 
 -- --------------------------------------------------------
 
@@ -159,10 +158,10 @@ INSERT INTO `demo_sub` VALUES(5, '2010-11-04 23:23:21', 'Options 4', 3);
 --
 
 CREATE TABLE `group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `description` text character set utf8 collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 --
@@ -179,10 +178,10 @@ INSERT INTO `group` VALUES(2, 'Administrator', 'Client administrator, can edit u
 --
 
 CREATE TABLE `group_permission` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `group_id` int(11) NOT NULL,
   `permission_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `group_id` (`group_id`,`permission_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -209,11 +208,11 @@ INSERT INTO `group_permission` VALUES(11, 2, 10);
 --
 
 CREATE TABLE `permission` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `permission` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `name` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL auto_increment,
+  `permission` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `name` varchar(150) collate utf8_unicode_ci NOT NULL,
+  `description` varchar(500) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -238,16 +237,17 @@ INSERT INTO `permission` VALUES(10, 'claeroadmin/useradmin/*', 'Database Admin -
 --
 
 CREATE TABLE `session` (
-  `session_id` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
+  `session_id` varchar(24) collate utf8_unicode_ci NOT NULL,
   `last_active` int(10) unsigned NOT NULL,
-  `contents` longtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`session_id`),
+  `contents` longtext collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`session_id`),
   KEY `last_active` (`last_active`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `session`
 --
+
 
 -- --------------------------------------------------------
 
@@ -256,19 +256,19 @@ CREATE TABLE `session` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `expiry_date` datetime NOT NULL,
-  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `password` char(50) COLLATE utf8_unicode_ci NOT NULL,
-  `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(100) collate utf8_unicode_ci NOT NULL,
+  `password` char(50) collate utf8_unicode_ci NOT NULL,
+  `first_name` varchar(100) collate utf8_unicode_ci NOT NULL,
+  `last_name` varchar(100) collate utf8_unicode_ci NOT NULL,
   `active_flag` tinyint(1) NOT NULL,
   `login_count` smallint(6) NOT NULL,
   `last_login` datetime NOT NULL,
   `failed_login_count` mediumint(9) NOT NULL,
   `last_failed_login` datetime NOT NULL,
-  `reset_token` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
+  `reset_token` char(32) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `username` (`username`),
   KEY `password` (`password`),
   KEY `active_flag` (`active_flag`)
@@ -278,9 +278,9 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` VALUES(1, '0000-00-00 00:00:00', 'admin@admin.com', 'e10adc3949ba59abbe56e057f20f883e', 'Admin', 'Account', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', '');
-INSERT INTO `user` VALUES(2, '0000-00-00 00:00:00', 'craig@nakamoto.ca', '0d107d09f5bbe40cade3de5c71e9e9b7', 'Craig', 'Nakamoto', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', '');
-INSERT INTO `user` VALUES(3, '0000-00-00 00:00:00', 'user@example.com', 'd41d8cd98f00b204e9800998ecf8427e', 'User', '#1', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', '');
+INSERT INTO `user` VALUES(1, '0000-00-00 00:00:00', 'admin@admin.com', '4668682c67aa41e5abeefb3989dbf7879d14290099bdd892f8', 'Admin', 'Admin', 1, 204, '2010-11-16 17:28:31', 0, '2010-11-16 09:01:28', '');
+INSERT INTO `user` VALUES(2, '0000-00-00 00:00:00', 'craig@nakamoto.ca', '8804290dc243b37f4e3f83e53ff5a9334aabd0e4446b5e86f0', 'Craig', 'Nakamoto', 1, 22, '2010-11-16 10:55:17', 0, '2010-11-16 08:56:42', 'URmnrwYzAZFKX7weX3mynb2gARSbJwFa');
+INSERT INTO `user` VALUES(3, '0000-00-00 00:00:00', 'user@example.com', '526faa0fb5f3e551a8b6904fd6108a92ffb2392f6751b27b7e', 'User1', '#1', 1, 0, '0000-00-00 00:00:00', 1, '2010-09-28 01:20:07', 'fFdMuP7GtKs4CYzbzKJJhruhbZgFuCPR');
 
 -- --------------------------------------------------------
 
@@ -289,10 +289,10 @@ INSERT INTO `user` VALUES(3, '0000-00-00 00:00:00', 'user@example.com', 'd41d8cd
 --
 
 CREATE TABLE `user_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `user_id` (`user_id`,`group_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -300,8 +300,8 @@ CREATE TABLE `user_group` (
 -- Dumping data for table `user_group`
 --
 
-INSERT INTO `user_group` VALUES(1, 1, 2);
-INSERT INTO `user_group` VALUES(2, 2, 2);
+INSERT INTO `user_group` VALUES(1, 1, 1);
+INSERT INTO `user_group` VALUES(2, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -310,12 +310,12 @@ INSERT INTO `user_group` VALUES(2, 2, 2);
 --
 
 CREATE TABLE `user_token` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `date_created` datetime NOT NULL,
   `date_expired` datetime NOT NULL,
   `user_id` int(11) NOT NULL,
-  `token` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
+  `token` varchar(35) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`),
   KEY `date_expired` (`date_expired`,`user_id`,`token`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
