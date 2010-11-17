b585c77650c70b965f2fb7a54f2b70f357eeb91b
Darryl Hein <dhein@xmmedia.com>
2010-11-17 18:49:27 +0000

    tweaks to reset permissions so it doesn't need to be run by root

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -1,13 +1,17 @@
-chown templat4 -R *
-chgrp templat4 -R *
-chgrp apache -R uploads
-chgrp apache -R html/uploads
-chgrp apache -R logs
-chgrp apache -R cache
+#chown templat4 -R *
+#chgrp templat4 -R *
+#chgrp apache -R uploads
+#chgrp apache -R html/uploads
+#chgrp apache -R logs
+#chgrp apache -R cache
 chmod g+w -R uploads
+chmod o+w -R uploads
 chmod g+w -R html/uploads
+chmod o+w -R html/uploads
 chmod g+w -R logs
+chmod o+w -R logs
 chmod g+w -R cache
-cd html
-find . -type f -exec chmod 644 {} \;
-find . -type d -exec chmod 755 {} \;
+chmod o+w -R cache
+#cd html
+#find . -type f -exec chmod 644 {} \;
+#find . -type d -exec chmod 755 {} \;
