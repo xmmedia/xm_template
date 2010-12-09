a2200fc122da2e658faa262c952007e5ff7d02f7
Darryl Hein <dhein@xmmedia.com>
2010-12-09 06:21:42 +0000

    adding recaptcha key

--- html/index.php
+++ html/index.php
@@ -59,11 +59,11 @@ switch ($server_id) {
 		define('URL_ROOT', HTTP_PROTOCOL . '://template4.claero.com');
 		define('ABS_ROOT', '/home/templat4/template4.claero.com');
 		define('ANALYTICS_ID', 'UA-468095-28'); // UA-468095-28 is for template4.claero.com
-		define('RECAPTCHA_PUBLIC_KEY', '');
-		define('RECAPTCHA_PRIVATE_KEY', '');
+		define('RECAPTCHA_PUBLIC_KEY', '6LfEc78SAAAAAK0dnSQ7bu9NgcEA-PSku-7qR2w8');
+		define('RECAPTCHA_PRIVATE_KEY', '6LfEc78SAAAAAAOLItfy5lH_x-43dZxXF-cCblC6');
 		define('DATABASE_DEFAULT', 'development');
 		define('SESSION_TYPE', 'database');
-		define('ADMIN_EMAIL', 'claero-support@claero.com');
+		define('ADMIN_EMAIL', 'darryl.hein@claero.com');
 		break;
 
     // Local Development Dan Hulton
