a1816e947a587929fd8394f1a6f80719b0d01ff8
Dan Hulton <dan@danhulton.com>
2010-12-13 19:55:56 +0000

    Created the db_change_log.sql file for making changes to create_mysql.sql file.

---
+++ sql/db_change_log.sql
@@ -0,0 +1,5 @@
+# 2010/12/13 - BDH: Issue #37 ----------------------------------------------------------------------
+
+ALTER TABLE `templat4_core`.`user` MODIFY COLUMN `id` INT(11) NOT NULL,
+ ADD COLUMN `force_update_password_flag` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `reset_token`,
+ ADD COLUMN `force_update_profile_flag` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `force_update_password_flag`;
