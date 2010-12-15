34158789b1f22221a1c63863d49bc60f15c10939
Darryl Hein <dhein@xmmedia.com>
2010-12-15 05:05:57 +0000

    removing extra change log SQL removing default values from user table in create sql (as the defualt will be 0 anyway)

--- sql/db_change_log.sql
+++ sql/db_change_log.sql
@@ -1,5 +1,4 @@
 # 2010/12/13 - BDH: Issue #37 ----------------------------------------------------------------------
 
-ALTER TABLE `user` MODIFY COLUMN `id` INT(11) NOT NULL,
- ADD COLUMN `force_update_password_flag` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `reset_token`,
- ADD COLUMN `force_update_profile_flag` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `force_update_password_flag`;
+ALTER TABLE `user` ADD `force_update_password_flag` TINYINT( 1 ) UNSIGNED NOT NULL ,
+ADD `force_update_profile_flag` TINYINT( 1 ) UNSIGNED NOT NULL ;
