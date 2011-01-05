7b0ad0afe120889386970c46e3940fb39162b730
Darryl Hein <dhein@xmmedia.com>
2011-01-04 23:42:24 -0700

    removing second database connection as PDO has a lot of restrictions and probably can't be used with Kohana

--- application/config/database.php
+++ application/config/database.php
@@ -28,30 +28,4 @@ return array(
 		'caching'      => FALSE,
 		'profiling'    => TRUE,
 	),
-	'development-alternate' => array(
-		'type'       => 'pdo',
-		'connection' => array(
-			/**
-			 * The following options are available for PDO:
-			 *
-			 * string   dsn         Data Source Name
-			 * string   username    database username
-			 * string   password    database password
-			 * boolean  persistent  use persistent connections?
-			 */
-			'dsn'        => 'mysql:host=localhost;dbname=templat4_core',
-			'username'   => 'templat4_core',
-			'password'   => 'template467',
-			'persistent' => FALSE,
-		),
-		/**
-		 * The following extra options are available for PDO:
-		 *
-		 * string   identifier  set the escaping identifier
-		 */
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
-	),
 );
