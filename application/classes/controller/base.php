51b1fd0c37b23cc6a88ecde90e0045099c460742
Darryl Hein <dhein@xmmedia.com>
2011-07-29 16:34:08 -0600

    adding request log functionality (from modules/xmmedia)

---
+++ application/classes/controller/base.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+class Controller_Base extends Controller_cl4_Base {
+	/**
+	* Called before the action
+	* Does everything else in the parent before()'s, but also logs the request.
+	*
+	*/
+	public function before() {
+		try {
+			Model_Request_Log::store_request();
+		} catch (Exception $e) {
+			Kohana_Exception::caught_handler($e, FALSE, FALSE);
+		}
+
+		parent::before();
+	} // function before
+}
