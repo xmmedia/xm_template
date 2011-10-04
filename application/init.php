54e311df791a603a3082420f64003b19cf3e6ec2
Darryl Hein <dhein@xmmedia.com>
2011-10-04 00:38:27 -0600

    correcting the config

--- application/init.php
+++ application/init.php
@@ -19,8 +19,8 @@ if ( ! isset($_SERVER['SERVER_PORT'])) {
 }
 define('URL_ROOT', HTTP_PROTOCOL . '://template.xmmedia.net');
 define('ABS_ROOT', '/home/template/template4.xmmedia.net');
-define('ANALYTICS_ID', 'UA-17170806-9'); // UA-468095-28 is for template4.claero.com
-define('RECAPTCHA_PUBLIC_KEY', '66Lc1xMgSAAAAAI7B-eRAahlvGQS2yKB16o5qBP3X');
+define('ANALYTICS_ID', 'UA-17170806-9'); // UA-17170806-9 is for template4.xmmedia.net
+define('RECAPTCHA_PUBLIC_KEY', '6Lc1xMgSAAAAAI7B-eRAahlvGQS2yKB16o5qBP3X');
 define('RECAPTCHA_PRIVATE_KEY', '6Lc1xMgSAAAAAMh1B01a4esghv32GyG36rZM6-VA');
 define('DATABASE_DEFAULT', 'development');
 define('SESSION_TYPE', 'database');
