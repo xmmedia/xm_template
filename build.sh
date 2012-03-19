f022657fde090e2c0d91304b32311f826bbd3af9
Darryl Hein <dhein@xmmedia.com>
2012-03-19 00:41:33 -0600

    updating the build script for the 1140 grid

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 # creates and minified production JS and CSS files
 
 echo "Creating html/css/production.min.css"
-cat html/css/reset.css html/cl4/css/cl4.css html/css/base.css > html/css/production.css
+cat html/css/1140.css html/css/normalize.css html/cl4/css/cl4.css html/css/base.css > html/css/production.css
 java -jar /usr/local/bin/yuicompressor-2.4.6.jar html/css/production.css -o html/css/production.min.css --line-break 8000
 rm html/css/production.css
+
+echo "Creating html/css/public.min.css"
+cat html/css/1140.css html/css/normalize.css html/cl4/css/cl4.css html/css/base.css html/css/public.css > html/css/public_temp.css
+java -jar /usr/local/bin/yuicompressor-2.4.6.jar html/css/public_temp.css -o html/css/public.min.css --line-break 8000
+rm html/css/public_temp.css
 
 echo "Creating html/js/production.min.js"
 cat html/cl4/js/cl4.js html/cl4/js/ajax.js html/js/base.js > html/js/production.js
