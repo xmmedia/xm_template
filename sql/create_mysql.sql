b799961f3bd7f1acbc7b83488d73682f54b545f2
Darryl Hein <dhein@xmmedia.com>
2012-04-16 22:48:30 -0600

    correcting the IDs in the group permission table

--- sql/create_mysql.sql
+++ sql/create_mysql.sql
@@ -675,17 +675,17 @@ INSERT INTO `permission` VALUES(NULL, 'useradmin/group/delete', 'User Admin - Gr
 
 
 -- Assign the User Admin and DB Change permissions to the default groups
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(14, 1, 25);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(15, 1, 28);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(16, 1, 29);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(17, 1, 26);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(18, 1, 27);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(19, 1, 30);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(20, 1, 32);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(21, 1, 36);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(22, 1, 33);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(23, 1, 34);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(24, 1, 35);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(25, 1, 31);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(26, 1, 24);
-INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(27, 1, 23);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(15, 1, 25);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(16, 1, 28);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(17, 1, 29);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(18, 1, 26);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(19, 1, 27);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(20, 1, 30);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(21, 1, 32);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(22, 1, 36);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(23, 1, 33);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(24, 1, 34);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(25, 1, 35);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(26, 1, 31);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(27, 1, 24);
+INSERT INTO `group_permission` (`id`, `group_id`, `permission_id`) VALUES(28, 1, 23);
