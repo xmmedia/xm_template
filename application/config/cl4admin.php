38779b2b98a058b6b74667f2b7be1d77c94a6275
Darryl Hein <dhein@xmmedia.com>
2012-06-16 23:53:28 -0600

    updating modules; adding the change script

--- application/config/cl4admin.php
+++ application/config/cl4admin.php
@@ -2,6 +2,7 @@
 
 return array(
 	'model_list' => array(
+		'change_script' => 'Change Scripts',
 		// remove the user list from the admin
 		'user_admin' => '',
 	),
---
+++ application/config/xm.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'routes' => array(
+		// enable the change_script route
+		'change_script' => TRUE,
+	),
+);
