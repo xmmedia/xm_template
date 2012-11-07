f6e589c20b52c526a89fe63d316476d887e4871f
Darryl Hein <dhein@xmmedia.com>
2012-11-06 23:52:47 -0700

    comment correction
    periods

--- application/classes/Kohana.php
+++ application/classes/Kohana.php
@@ -2,9 +2,9 @@
 
 class Kohana extends Kohana_Core {
 	/**
-	 * @var  array   Include paths that are used to find files
-	 * Adds CL4_PATH to the list of default paths to use when looking for files
-	 * Used when finding the exception handler
+	 * @var  array   Include paths that are used to find files.
+	 * Adds CL4_PATH to the list of default paths to use when looking for files.
+	 * Used when finding the exception handler.
 	 */
 	protected static $_paths = array(APPPATH, CL4_PATH, SYSPATH);
 }
