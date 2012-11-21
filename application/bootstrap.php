cbd2f90b01f857d2a7ae6d5a1a07daeefb62c48b
Darryl Hein <dhein@xmmedia.com>
2012-11-21 01:43:06 -0700

    public route will now take anything that doesn't match anything else

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -151,7 +151,7 @@ if (Kohana::$environment == Kohana::DEVELOPMENT && Auth::instance()->logged_in()
  */
 
 // routes for "static" pages without a sub folder
-Route::set('public', '(<action>)', array('action' => '|'))
+Route::set('public', '(<action>)')
 	->defaults(array(
 		'controller' => 'public',
 		'action' => 'index',
