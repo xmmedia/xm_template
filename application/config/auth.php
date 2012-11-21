daa4f4bc4ce882d0de9e71d1b9a79a5202c6f007
Darryl Hein <dhein@xmmedia.com>
2012-11-21 01:43:18 -0700

    changing useradmin to user_admin

--- application/config/auth.php
+++ application/config/auth.php
@@ -4,5 +4,5 @@ return array(
 	// hash key for Kohana 3.1 hashing
 	'hash_key' => 'H,Mi8&kc|Zs?{4=,V_,0MS%(g,3AH=[DXUak\+7i^qHg5g)\`+pQ!7y4Ij2KiUA',
 
-	'default_login_redirect' => 'useradmin',
+	'default_login_redirect' => 'user_admin',
 );
