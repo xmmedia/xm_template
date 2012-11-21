5b1cef33317a0a61135abca0431242f68e40e2f6
Darryl Hein <dhein@xmmedia.com>
2012-11-21 01:42:43 -0700

    removing default exception handling

--- application/classes/Controller/Public.php
+++ application/classes/Controller/Public.php
@@ -10,11 +10,6 @@ class Controller_Public extends Controller_CL4_Public {
 	 * @return void
 	 */
 	public function action_index() {
-		try {
-			$this->template->body_html = View::factory('pages/index');
-		} catch (Exception $e) {
-			Kohana_Exception::caught_handler($e);
-			$this->error_500();
-		}
+		$this->template->body_html = View::factory('pages/index');
 	} // function action_index
 }
