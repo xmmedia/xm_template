c157aad1e951f46f0d892c1f603af9952b13472d
Darryl Hein <dhein@xmmedia.com>
2012-11-22 23:54:29 -0700

    adding the change script config

---
+++ application/config/change_script.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'default' => array(
+		// array of databases that all the changes need to be applied to
+		'databases' => array('templat1_main'),
+	),
+);
