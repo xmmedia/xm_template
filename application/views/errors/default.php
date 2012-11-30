be49e8ec475836d595b13d50cb24f753fed86877
Darryl Hein <dhein@xmmedia.com>
2012-11-29 18:55:07 -0700

    adding a default error view to allow for the customization of the 404

---
+++ application/views/errors/default.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php
+if ( ! empty($page_title)) {
+	echo HTML::chars($page_title);
+} ?></title>
+<?php
+$styles = array(
+	'css/base.css' => NULL,
+	'css/public.css' => NULL,
+);
+foreach ($styles as $file => $type) {
+	echo TAB, HTML::style($file, array('media' => $type)), EOL;
+}
+
+echo TAB, HTML::script('js/modernizr.min.js'), EOL;
+?>
+</head>
+
+<body>
+<?php
+// header: menus, logos, etc
+echo View::factory('public/header')
+	->set($kohana_view_data);
+?>
+<div class="grid">
+	<h1><?php echo HTML::chars($title); ?></h1>
+	<p><?php echo HTML::chars($message); ?></p>
+</div>
+<?php
+// the footer
+echo View::factory('public/footer')
+	->set($kohana_view_data);
+// debug: debug output; detects debug mode within view
+echo View::factory('base/debug')
+		->set($kohana_view_data); ?>
+</body>
+</html>
