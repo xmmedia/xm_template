176536286988511f0a826b91e04c07e721e63521
Darryl Hein <dhein@xmmedia.com>
2013-01-27 22:45:45 -0700

    removing build script
    everything is now in codekit

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# creates and minified production JS files
-
-#echo "Creating html/js/production.min.js"
-#cat html/cl4/js/cl4.js html/cl4/js/ajax.js html/js/base.js > html/js/production.js
-#java -jar /usr/local/bin/yuicompressor-2.4.6.jar html/js/production.js -o html/js/production.min.js --line-break 8000
-#rm html/js/production.js
-
-exit 0
