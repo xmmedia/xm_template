c090ccae40f74fdfd2ce3c2a124a4e6c239de6fe
Darryl Hein <dhein@xmmedia.com>
2013-07-27 21:10:22 -0600

    moving the top nav JS to private.js

--- html/js/base.js
+++ html/js/base.js
@@ -88,35 +88,4 @@ $.ajaxSetup({ cache: false });
 $(function() {
 	$('[autofocus]').autofocus();
 	$('.numeric').numeric();
-
-	// sub nav
-	$('.top_nav_wrapper').on('click', 'li.has_subnav > a', function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-
-		var $link = $(this),
-			$sub_nav = $link.parent().find('.sub_nav'),
-			link_offset,
-			link_width;
-		if ($sub_nav.css('visibility') !== 'hidden') {
-			$sub_nav.css({ visibility: 'hidden' });
-			$link.removeClass('clicked');
-		} else {
-			$('.top_nav_wrapper li.has_subnav > a.clicked').click();
-			link_offset = $link.offset();
-			link_width = $link.width();
-			if ($sub_nav.hasClass('right')) {
-				$sub_nav.css({ right: ($(window).width() - (link_offset.left + link_width + 39)), top : ($link.parent().innerHeight()), visibility: 'visible' });
-			} else {
-				$sub_nav.css({ left: (link_offset.left - 5), top : ($link.parent().innerHeight()), visibility: 'visible' });
-			}
-			if ($sub_nav.width() < link_width + 19) {
-				$sub_nav.css('width', link_width + 19 + 'px');
-			}
-			$link.addClass('clicked');
-			$sub_nav.bind('clickoutside', function() {
-				$('.top_nav_wrapper li.has_subnav > a.clicked').click();
-			});
-		}
-	});
 });
