2c16f3723f0f3b40b31dc35cb73ffb32a3024937
Darryl Hein <dhein@xmmedia.com>
2014-01-14 11:09:19 -0700

    "installing" & configuring grunt

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			options: {
+				mangle: false
+			},
+			build: {
+				files: {
+					'html/js/private.min.js':
+					[
+						'html/js/jquery.outside.min.js',
+						'html/xm/js/xm.js',
+						'html/xm/js/ajax.js',
+						'html/js/base.js'
+					]
+				}
+			}
+		},
+
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb'
+				}
+			}
+		},
+
+		autoprefixer: {
+			multiple_files: {
+				expand: true,
+				flatten: true,
+				src: 'html/css/*.css', // -> src/css/file1.css, src/css/file2.css
+				dest: 'html/css/' // -> dest/css/file1.css, dest/css/file2.css
+			},
+		},
+
+		watch: {
+			scripts: {
+				files: ['html/js/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+
+			css: {
+				files: ['html/css/sass/*.scss'],
+				tasks: ['compass'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['uglify', 'compass', 'autoprefixer']);
+};
