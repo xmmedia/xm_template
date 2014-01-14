e675266fc48e0480217493bbd0a7d2c68e5e9cee
Darryl Hein <dhein@xmmedia.com>
2014-01-14 11:11:36 -0700

    removing old scripts
    most have been added to the wiki

--- compass_compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# compiles the SASS files for the project and the libraries (just in case)
-cd html/xm/ && compass compile . --time --environment development && cd ../..
-compass compile . --time --environment development
--- compass_watch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# watches the current project, including the xm library
-compass watch . --time --environment development -I html/xm/css/sass/
